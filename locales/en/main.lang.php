1bab3d3713ac12fd73aceb16c5815a54232c685e
franck <carnet.franck.paul@gmail.com>
2025-07-08 14:20:15 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Embed Media'] = '';
+L10n::$locales['Embed external media from Internet'] = '';
---
+++ locales/fr/main.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Embed Media'] = 'Intégrer des médias';
+L10n::$locales['Embed external media from Internet'] = 'Intégrer des médias externes à partir d\'Internet';
