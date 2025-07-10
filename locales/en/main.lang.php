9094a4ea650e85a99691ca69181f0cfa3f12544a
franck <carnet.franck.paul@gmail.com>
2025-07-10 12:05:19 +0200

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -12,3 +12,13 @@ use Dotclear\Helper\L10n;
 
 L10n::$locales['Embed Media'] = '';
 L10n::$locales['Embed external media from Internet'] = '';
+L10n::$locales['Embed external media'] = '';
+L10n::$locales['URL'] = '';
+L10n::$locales['Page URL:'] = '';
+L10n::$locales['URL cannot be empty.'] = '';
+L10n::$locales['Alignment'] = '';
+L10n::$locales['Media alignment:'] = '';
+L10n::$locales['None'] = '';
+L10n::$locales['Left'] = '';
+L10n::$locales['Right'] = '';
+L10n::$locales['Center'] = '';
--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,3 +12,13 @@ use Dotclear\Helper\L10n;
 
 L10n::$locales['Embed Media'] = 'Intégrer des médias';
 L10n::$locales['Embed external media from Internet'] = 'Intégrer des médias externes à partir d\'Internet';
+L10n::$locales['Embed external media'] = 'Intégrer un média externe';
+L10n::$locales['URL'] = 'URL';
+L10n::$locales['Page URL:'] = 'URL de la page :';
+L10n::$locales['URL cannot be empty.'] = 'L\'URL ne peut pas être vide.';
+L10n::$locales['Alignment'] = 'Alignement';
+L10n::$locales['Media alignment:'] = 'Alignement du média :';
+L10n::$locales['None'] = 'Aucun';
+L10n::$locales['Left'] = 'Gauche';
+L10n::$locales['Right'] = 'Droite';
+L10n::$locales['Center'] = 'Centre';
