419c1c8d3fc572251df852f35a052f0744e7c9f4
franck <carnet.franck.paul@gmail.com>
2025-09-06 22:16:20 +0200

    DC 2.36 compliance

--- src/FrontendUrl.php
+++ src/FrontendUrl.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
 namespace Dotclear\Plugin\EmbedMedia;
 
 use Dotclear\App;
-use Dotclear\Core\Frontend\Url;
+use Dotclear\Core\Url;
 use Dotclear\Helper\Network\Http;
 use Dotclear\Helper\Network\HttpClient;
 
