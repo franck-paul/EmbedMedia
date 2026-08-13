c6324850ffe54ec11bf5b5a8b30fbba7ca83762a
franck <contact@open-time.net>
2026-08-13 16:40:25 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,28 +11,32 @@
  * @copyright Franck Paul
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'EmbedMedia',
-    'Embed external media from Internet',
-    'Franck Paul',
-    '3.0',
-    [
-        'date'     => '2026-08-03T09:54:22+0200',
-        'requires' => [
-            ['core', '2.39'],
-            ['TemplateHelper'],
-        ],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
-        'settings'    => [
-            'self' => false,
-            'blog' => '#params.embed_media',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=EmbedMedia',
-        'support'    => 'https://github.com/franck-paul/EmbedMedia',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/EmbedMedia/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'EmbedMedia',
+        'Embed external media from Internet',
+        'Franck Paul',
+        '3.0',
+        [
+            'date'     => '2026-08-03T09:54:22+0200',
+            'requires' => [
+                ['core', '2.39'],
+                ['TemplateHelper'],
+            ],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
+            'settings'    => [
+                'self' => false,
+                'blog' => '#params.embed_media',
+            ],
+
+            'details'    => 'https://open-time.net/?q=EmbedMedia',
+            'support'    => 'https://github.com/franck-paul/EmbedMedia',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/EmbedMedia/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
